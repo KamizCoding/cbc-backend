e5b67f2b09571a9e2f8ca56047d38d190babd769
Lukmaan Kamiz <m.lukmaankamiz@gmail.com>
2024-11-27 08:09:46 +0530

    Added new peatures to product

--- models/products.js
+++ models/products.js
@@ -3,7 +3,9 @@ import mongoose from "mongoose"
 const productsSchema = mongoose.Schema({
     name : String,
     price : Number,
-    description : String
+    description : String,
+    discount : Number,
+    lastPrice : Number
   })
 
   const Products = mongoose.model("products", productsSchema)
