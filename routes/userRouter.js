57baa9a2438e50b24f4e276d5c4a3c4132f99845
Lukmaan Kamiz <m.lukmaankamiz@gmail.com>
2024-11-27 08:05:57 +0530

    Added new routes to userRouter

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { newUser, userLogin} from "../controllers/userController.js";
+import { delUser, listUser, newUser, userLogin} from "../controllers/userController.js";
 
 //Create studentRouter
 const userRouter = express.Router();
@@ -8,5 +8,8 @@ userRouter.post('/', newUser)
 
 userRouter.post('/login', userLogin)
 
+userRouter.get('',listUser)
+
+userRouter.delete('',delUser)
 
 export default userRouter;
