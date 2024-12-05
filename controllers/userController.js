0d17ff55a05dee5faa5d97d6b9914959683b629f
Lukmaan Kamiz <m.lukmaankamiz@gmail.com>
2024-12-05 08:41:47 +0530

    Added admin and customer check function to userController➕

--- controllers/userController.js
+++ controllers/userController.js
@@ -127,4 +127,28 @@ export async function delUser(req,res){
     }
 }
 
+export function isAdmin(req){
+    if(req.user == null){
+        return false
+    }
+
+    if(req.user.type != "admin"){
+        return false
+    }
+
+    return true
+}
+
+export function isCustomer(req){
+    if(req.user == null){
+        return false
+    }
+
+    if(req.user.type != "customer"){
+        return false
+    }
+
+    return true
+}
+
 
