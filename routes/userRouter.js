f0970c0ef3016aaf902f8a8c5e1283b24de55e72
Lukmaan Kamiz <m.lukmaankamiz@gmail.com>
2025-02-26 10:36:36 +0530

    Plugged New Routes to UserRouter🔌🫚

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { blockUser, delUser, getUser, googleLogin, listUser, newUser, updateUser, userLogin} from "../controllers/userController.js";
+import { blockUser, delUser, getUser, getUserActivity, googleLogin, listUser, logoutUser, newUser, updateUser, userLogin} from "../controllers/userController.js";
 
 //Create studentRouter
 const userRouter = express.Router();
@@ -7,6 +7,8 @@ const userRouter = express.Router();
 userRouter.post('/', newUser)
 
 userRouter.post('/login', userLogin)
+
+router.post("/logout", logoutUser);
 
 userRouter.get('/userdetail',getUser) 
 
@@ -19,5 +21,7 @@ userRouter.post("/google", googleLogin)
 userRouter.put("/", blockUser)
 
 userRouter.put('/update/:email', updateUser)
+
+router.get("/activity", getUserActivity);
 
 export default userRouter;
